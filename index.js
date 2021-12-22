88c7a5f4accd63331c5e4db54172ac112b8ff02f
Iglisson Ruan <iglioliveira87@outlook.com>
2021-12-22 15:39:59 -0300

    :package: Consumindo API para verificar a cotação do Dolar

--- index.js
+++ index.js
@@ -1,15 +1,24 @@
 import ApiGitHub from "./src/services/ApiGitHub.js";
 import ApiViaCep from "./src/services/ApiViaCep.js";
 import ApiJasPlaHol from "./src/services/ApiJasPlaHol.js";
+import ApiCotacaoDolar from "./src/services/ApiCotacaoDolar.js";
 
-ApiViaCep.getAdress("68610000")
-    .then(v => { console.log(v) })
-    .catch( err => { console.error(err)});
+// Chamada
+// ApiViaCep.getAdress("68610000")
+//     .then(v => { console.log(v) })
+//     .catch( err => { console.error(err)});
 
-ApiGitHub.getUserData("iglisson")
-    .then( v => { console.log(v) })
-    .catch( err => { console.error(err)});
+// Chamada
+// ApiGitHub.getUserData("iglisson")
+//     .then( v => { console.log(v) })
+//     .catch( err => { console.error(err)});
 
-ApiJasPlaHol.getUser("2")
-    .then(v => { console.log(v) })
-    .catch( err => { console.error(err)});
+// Chamada
+// ApiJasPlaHol.getUser("2")
+//     .then(v => { console.log(v) })
+//     .catch( err => { console.error(err)});
+
+// Chamada
+ApiCotacaoDolar.getValue()
+    .then(v => console.log(`Compra: R$ ${v.bid} | Venda: R$ ${v.ask}`))
+    .catch( err => console.error(err))
